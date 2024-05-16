b16bebe276dcd9bd5ee7525f5ffcca7198d98993
Kristina Dudjak <dudjakkristina@gmail.com>
2024-05-16 23:03:00 +0200

    router setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
